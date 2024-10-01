2349c6f1c514ea2bf320d10e84c4bd54fb47b5fc
stukenvitalii <stukenvitalii@gmail.com>
2024-10-01 18:12:38 +0300

    add some configuration files

---
+++ user-service/src/main/java/org/tinkoff/userservice/configuration/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package org.tinkoff.userservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+public record ApplicationConfig(
+        String orderServiceUrl,
+        String restaurantServiceUrl
+) {}
