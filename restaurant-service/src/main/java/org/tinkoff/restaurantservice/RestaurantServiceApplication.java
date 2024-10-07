904ff57c3398c1c9206e0c8e7cbc925d6a16ac9d
stukenvitalii <stukenvitalii@gmail.com>
2024-10-07 19:39:22 +0300

    add ApplicationConfig

--- restaurant-service/src/main/java/org/tinkoff/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/org/tinkoff/restaurantservice/RestaurantServiceApplication.java
@@ -2,8 +2,11 @@ package org.tinkoff.restaurantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.tinkoff.restaurantservice.configuration.ApplicationConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
