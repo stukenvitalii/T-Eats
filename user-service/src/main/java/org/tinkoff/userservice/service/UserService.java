a2c4b180cc3df1bb0eef1715821ff655d53d8edc
stukenvitalii <stukenvitalii@gmail.com>
2024-11-14 14:42:31 +0300

    add UserService

--- user-service/src/main/java/org/tinkoff/userservice/service/UserService.java
+++ user-service/src/main/java/org/tinkoff/userservice/service/UserService.java
@@ -1,10 +1,18 @@
 package org.tinkoff.userservice.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.tinkoff.userservice.dto.UserDto;
+import org.tinkoff.userservice.dto.mapper.UserMapper;
 import org.tinkoff.userservice.entity.User;
 import org.tinkoff.userservice.repository.UserRepository;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,25 +20,74 @@ import java.util.Optional;
 @Service
 public class UserService {
 
+    private final UserMapper userMapper;
+
     private final UserRepository userRepository;
 
-    public void deleteById(Integer id) {
-        userRepository.deleteById(id);
+    private final ObjectMapper objectMapper;
+
+    public List<UserDto> getList() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public UserDto getOne(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userMapper.toDto(userOptional.orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id))));
     }
 
-    public Optional<User> findById(Integer id) {
-        return userRepository.findById(id);
+    public List<UserDto> getMany(List<Integer> ids) {
+        List<User> users = userRepository.findAllById(ids);
+        return users.stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 
-    public User save(User entity) {
-        return userRepository.save(entity);
+    public UserDto create(UserDto dto) {
+        User user = userMapper.toEntity(dto);
+        User resultUser = userRepository.save(user);
+        return userMapper.toDto(resultUser);
     }
 
-    public List<User> saveAll(Iterable<User> entities) {
-        return userRepository.saveAll(entities);
+    public UserDto patch(Integer id, JsonNode patchNode) throws IOException {
+        User user = userRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+
+        UserDto userDto = userMapper.toDto(user);
+        objectMapper.readerForUpdating(userDto).readValue(patchNode);
+        userMapper.updateWithNull(userDto, user);
+
+        User resultUser = userRepository.save(user);
+        return userMapper.toDto(resultUser);
+    }
+
+    public List<Integer> patchMany(List<Integer> ids, JsonNode patchNode) throws IOException {
+        Collection<User> users = userRepository.findAllById(ids);
+
+        for (User user : users) {
+            UserDto userDto = userMapper.toDto(user);
+            objectMapper.readerForUpdating(userDto).readValue(patchNode);
+            userMapper.updateWithNull(userDto, user);
+        }
+
+        List<User> resultUsers = userRepository.saveAll(users);
+        return resultUsers.stream()
+                .map(User::getId)
+                .toList();
+    }
+
+    public UserDto delete(Integer id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            userRepository.delete(user);
+        }
+        return userMapper.toDto(user);
+    }
+
+    public void deleteMany(List<Integer> ids) {
+        userRepository.deleteAllById(ids);
     }
 }
