b985b220ac9a4798dc57c21647c3283835cc9d07
stukenvitalii <stukenvitalii@gmail.com>
2024-11-14 14:01:28 +0300

    INT -> BIGINT in all id's, rename tables

--- migrations/add_table_restaurants.sql
+++ migrations/add_table_restaurants.sql
@@ -1,7 +1,8 @@
-CREATE TABLE restaurants (
-                             id SERIAL PRIMARY KEY,            -- Unique identifier for each restaurant
-                             name VARCHAR(100) NOT NULL,       -- Name of the restaurant
-                             address VARCHAR(255) NOT NULL,    -- Address of the restaurant
-                             phone_number VARCHAR(15),         -- Phone number of the restaurant
-                             email VARCHAR(100)                -- Email of the restaurant
+CREATE TABLE restaurant
+(
+    id           SERIAL PRIMARY KEY,    -- Unique identifier for each restaurant
+    name         VARCHAR(100) NOT NULL, -- Name of the restaurant
+    address      VARCHAR(255) NOT NULL, -- Address of the restaurant
+    phone_number VARCHAR(15),           -- Phone number of the restaurant
+    email        VARCHAR(100)           -- Email of the restaurant
 );
