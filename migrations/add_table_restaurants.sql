fe8b9e499567c7100fdcd11c6da84e741243aa08
stukenvitalii <stukenvitalii@gmail.com>
2024-11-29 12:38:24 +0300

    update migrations

--- migrations/add_table_restaurants.sql
+++ migrations/add_table_restaurants.sql
@@ -1,4 +1,4 @@
-CREATE TABLE restaurant
+CREATE TABLE restaurants
 (
     id           SERIAL PRIMARY KEY,    -- Unique identifier for each restaurant
     name         VARCHAR(100) NOT NULL, -- Name of the restaurant
@@ -6,3 +6,9 @@ CREATE TABLE restaurant
     phone_number VARCHAR(15),           -- Phone number of the restaurant
     email        VARCHAR(100)           -- Email of the restaurant
 );
+
+INSERT INTO restaurants (name, address, phone_number, email)
+VALUES ('Restaurant 1',
+        'Address 1',
+        '1234567890',
+        'restaurant@eats.com');
