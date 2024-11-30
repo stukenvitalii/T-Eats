aceae75abcf5c3fa9e87f1877ec33a07a0d28825
stukenvitalii <stukenvitalii@gmail.com>
2024-11-30 21:21:35 +0300

    return Long in delete method, add getOrdersByUserId method

--- order-service/src/main/java/org/tinkoff/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/tinkoff/orderservice/controller/OrderController.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.tinkoff.orderservice.dto.CreateOrderRequest;
 import org.tinkoff.orderservice.dto.CreateOrderResponse;
+import org.tinkoff.orderservice.entity.Order;
 import org.tinkoff.orderservice.service.OrderService;
 
 import java.util.List;
@@ -20,6 +22,7 @@ import java.util.List;
 @RequestMapping("/rest/orders")
 @RequiredArgsConstructor
 @Tag(name = "Order Controller", description = "APIs for managing orders")
+@Validated
 public class OrderController {
 
     private final OrderService orderService;
@@ -37,7 +40,7 @@ public class OrderController {
             @ApiResponse(description = "Order details", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateOrderResponse.class))),
             @ApiResponse(description = "Order not found", responseCode = "404")
     })
-    public CreateOrderResponse getOrderById(@PathVariable Long id) {
+    public CreateOrderResponse getOrderById(@PathVariable("id") Long id) {
         return orderService.getOne(id);
     }
 
@@ -45,7 +48,7 @@ public class OrderController {
     @Operation(summary = "Get orders by IDs", responses = {
             @ApiResponse(description = "List of orders", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateOrderResponse.class)))
     })
-    public List<CreateOrderResponse> getOrdersByIds(@RequestParam List<Long> ids) {
+    public List<CreateOrderResponse> getOrdersByIds(@RequestParam("ids") List<Long> ids) {
         return orderService.getMany(ids);
     }
 
@@ -54,10 +57,6 @@ public class OrderController {
             @ApiResponse(description = "Order created", responseCode = "201", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateOrderResponse.class)))
     })
     public CreateOrderResponse createOrder(@RequestBody CreateOrderRequest orderRequest) {
-        log.info("Creating order from request: {}", orderRequest);
-        log.info("Creating order with restaurantId: {}", orderRequest.getRestaurantId());
-        log.info("Creating order with userId: {}", orderRequest.getUserId());
-        log.info("Creating order with totalAmount: {}", orderRequest.getTotalAmount());
         return orderService.create(orderRequest);
     }
 
@@ -66,7 +65,7 @@ public class OrderController {
             @ApiResponse(description = "Order patched", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateOrderResponse.class))),
             @ApiResponse(description = "Order not found", responseCode = "404")
     })
-    public CreateOrderResponse patchOrder(@PathVariable Long id, @RequestBody JsonNode patchNode) {
+    public CreateOrderResponse patchOrder(@PathVariable("id") Long id, @RequestBody JsonNode patchNode) {
         return orderService.patch(id, patchNode);
     }
 
@@ -74,7 +73,7 @@ public class OrderController {
     @Operation(summary = "Patch multiple orders by IDs", requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = "application/json", schema = @Schema(implementation = JsonNode.class))), responses = {
             @ApiResponse(description = "Orders patched", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Integer.class)))
     })
-    public List<Long> patchOrders(@RequestParam List<Long> ids, @RequestBody JsonNode patchNode) {
+    public List<Long> patchOrders(@RequestParam("ids") List<Long> ids, @RequestBody JsonNode patchNode) {
         return orderService.patchMany(ids, patchNode);
     }
 
@@ -83,7 +82,7 @@ public class OrderController {
             @ApiResponse(description = "Order deleted", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateOrderResponse.class))),
             @ApiResponse(description = "Order not found", responseCode = "404")
     })
-    public CreateOrderResponse deleteOrder(@PathVariable Long id) {
+    public Long deleteOrder(@PathVariable("id") Long id) {
         return orderService.delete(id);
     }
 
@@ -93,5 +92,10 @@ public class OrderController {
     })
     public void deleteOrders(@RequestParam List<Long> ids) {
         orderService.deleteMany(ids);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Order> getOrdersByUserId(@PathVariable Long userId) {
+        return orderService.getOrdersByUserId(userId);
     }
 }
