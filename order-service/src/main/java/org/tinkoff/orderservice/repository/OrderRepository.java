28f2dab068ee649d9dfb27678da56ce418a5dcc1
stukenvitalii <stukenvitalii@gmail.com>
2024-11-30 21:19:48 +0300

    add findUserById method

--- order-service/src/main/java/org/tinkoff/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/org/tinkoff/orderservice/repository/OrderRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.tinkoff.orderservice.entity.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+    List<Order> findByUserId(Long userId);
 }
