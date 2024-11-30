c4f6435b0baa263e74fb0c6bdccbda0a61614cf2
stukenvitalii <stukenvitalii@gmail.com>
2024-11-30 20:53:23 +0300

    change types, remove inserting data etc

--- migrations/add_table_users.sql
+++ migrations/add_table_users.sql
@@ -1,16 +1,9 @@
 CREATE TABLE users
 (
-    id         SERIAL PRIMARY KEY,           -- Unique identifier for each user
-    username   VARCHAR(50)  NOT NULL UNIQUE, -- Unique username for the user
-    password   VARCHAR(255) NOT NULL,        -- Password for the user (hashed)
-    email      VARCHAR(100) NOT NULL UNIQUE, -- Unique email for the user
-    first_name VARCHAR(50),                  -- User's first name
-    last_name  VARCHAR(50)                   -- User's last name
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- Unique identifier for each user
+    username   VARCHAR(50)  NOT NULL UNIQUE,                    -- Unique username for the user
+    password   VARCHAR(255) NOT NULL,                           -- Password for the user (hashed)
+    email      VARCHAR(100) NOT NULL UNIQUE,                    -- Unique email for the user
+    first_name VARCHAR(50),                                     -- User's first name
+    last_name  VARCHAR(50)                                      -- User's last name
 );
-
-INSERT INTO users (username, password, email, first_name, last_name)
-VALUES ('admin',
-        'admin',
-        'admin@admin.com',
-        'Admin',
-        'Admin');
