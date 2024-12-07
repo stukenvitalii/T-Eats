c42870fcbdbd7dba7bc0e02f78bc897764f27e31
stukenvitalii <stukenvitalii@gmail.com>
2024-12-07 16:36:47 +0300

    update migrations

--- migrations/add_table_dishes.sql
+++ migrations/add_table_dishes.sql
@@ -4,6 +4,7 @@ CREATE TABLE dishes
     name          VARCHAR(100)   NOT NULL,                  -- Name of the dish
     description   TEXT,                                     -- Description of the dish
     price         DECIMAL(10, 2) NOT NULL,                  -- Price of the dish
+    quantity      INTEGER        NOT NULL,                  -- Quantity of the dish
     restaurant_id BIGINT         NOT NULL,                  -- Identifier of the restaurant to which the dish belongs
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,      -- Timestamp for dish creation
     updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,      -- Timestamp for last update (set manually)
