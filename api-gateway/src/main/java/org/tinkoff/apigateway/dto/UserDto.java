e90d86fe6b3c6582ed013be8cc2b3d9ac7c5805e
stukenvitalii <stukenvitalii@gmail.com>
2024-12-15 14:35:57 +0300

    small fix

--- api-gateway/src/main/java/org/tinkoff/apigateway/dto/UserDto.java
+++ api-gateway/src/main/java/org/tinkoff/apigateway/dto/UserDto.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    private Long id;
     private String username;
     private String password;
     private Role role;
