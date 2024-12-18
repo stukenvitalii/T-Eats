c436423e5a23d043715667ff0c379f393bfbb492
stukenvitalii <stukenvitalii@gmail.com>
2024-12-18 16:07:46 +0300

    small changes

--- api-gateway/src/main/java/org/tinkoff/apigateway/controller/AuthController.java
+++ api-gateway/src/main/java/org/tinkoff/apigateway/controller/AuthController.java
@@ -1,7 +1,5 @@
 package org.tinkoff.apigateway.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +17,18 @@ public class AuthController {
 
     // Регистрация нового пользователя
     @PostMapping("/register")
-    @Operation(summary = "Register new user", responses = {
-            @ApiResponse(description = "User registered successfully", responseCode = "200"),
-            @ApiResponse(description = "Username already taken", responseCode = "400")
-    })
     public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
         return authService.register(registerRequest);
     }
 
     // Аутентификация (получение JWT токена)
     @PostMapping("/login")
-    @Operation(summary = "Authenticate user", responses = {
-            @ApiResponse(description = "User authenticated successfully", responseCode = "200"),
-            @ApiResponse(description = "Invalid credentials", responseCode = "400"),
-            @ApiResponse(description = "User not found", responseCode = "400")
-    })
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         return authService.authenticate(loginRequest);
     }
 
     // Сброс пароля
     @PostMapping("/reset-password")
-    @Operation(summary = "Reset user password", responses = {
-            @ApiResponse(description = "Password reset successfully", responseCode = "200")
-    })
     public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest,
                                            @RequestHeader("Authorization") String token) {
         return authService.resetPassword(resetPasswordRequest, token);
@@ -50,9 +36,6 @@ public class AuthController {
 
     // Выход (аннулирование токена)
     @PostMapping("/logout")
-    @Operation(summary = "Logout user", responses = {
-            @ApiResponse(description = "User logged out successfully", responseCode = "200")
-    })
     public ResponseEntity<?> logout(@RequestHeader("Authorization") String token) {
         authService.logout(token);
         return ResponseEntity.ok("Logged out successfully");
